3016e5fa29c6c3eb1ccdfc392df98b03b787bd79
Suraj <suraj.suraj.4.4.1996@gmail.com>
2024-03-26 11:41:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ def get_openai_response(user_input, message_history):
     return response.choices[0].message['content']
 st.image("https://camo.githubusercontent.com/59ef0255241368b37cddc9a9f13b0a943a864a7ab6ff6bd57e2641c41cf8c014/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f3939393134343535333535363734363235302f313037343835303832323639343930383031342f7275766e65745f61695f726f626f745f6170705f69636f6e5f5f41695f7374796c655f5f6d696e696d616c6973745f36346130306636342d666131342d346234302d383532612d3932623931613433393537302e706e67",  width=150)
 col1, col2 = st.columns((2, 1))
-col1.title("MSME bot demo")
+col1.title("VAA - bot demo")
 col1.markdown("<div class='small-header'> Ideate & Explore !</div>", unsafe_allow_html=True)
 
 # Store messages
